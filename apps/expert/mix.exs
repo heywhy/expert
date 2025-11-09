aa8bd84692222cfd763308815566d261430cd957
Hugo Baraúna <hugo.barauna@gmail.com>
2025-11-09 20:13:10 -0300

    fix: remove erts from extra_applications (#202)
    Running `mix release` was not working with Elixir 1.19.2, related to this
    https://github.com/elixir-lang/elixir/commit/bea1c6c58edff9c77a71ab1e94d096dd4496f3bb

--- apps/expert/mix.exs
+++ apps/expert/mix.exs
@@ -23,7 +23,7 @@ defmodule Expert.MixProject do
 
   def application do
     [
-      extra_applications: [:logger, :runtime_tools, :kernel, :erts, :observer],
+      extra_applications: [:logger, :runtime_tools, :kernel, :observer],
       mod: {Expert.Application, []}
     ]
   end
