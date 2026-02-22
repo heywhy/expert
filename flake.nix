e9217d802d02b52e647ec317b3797dd2213791f7
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-02-22 11:48:04 -0500

    ci: format nix and check in ci (#426)

--- flake.nix
+++ flake.nix
@@ -11,10 +11,9 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    { self, ... }@inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.beam-flakes.flakeModule ];
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.beam-flakes.flakeModule];
 
       systems = [
         "x86_64-darwin"
@@ -23,61 +22,61 @@
         "aarch64-linux"
       ];
 
-      perSystem =
-        { lib, pkgs, ... }:
-        let
-          beamPackages = pkgs.beamMinimal27Packages.extend (
-            _: prev: {
-              elixir = prev.elixir_1_17;
-            }
-          );
-        in
-        {
-          formatter = pkgs.nixfmt;
+      perSystem = {
+        lib,
+        pkgs,
+        ...
+      }: let
+        beamPackages = pkgs.beamMinimal27Packages.extend (
+          _: prev: {
+            elixir = prev.elixir_1_17;
+          }
+        );
+      in {
+        formatter = pkgs.nixfmt;
 
-          apps.update-deps =
-            let
-              script = pkgs.writeShellApplication {
-                name = "update-deps";
+        apps.update-deps = let
+          script = pkgs.writeShellApplication {
+            name = "update-deps";
 
-                runtimeInputs = [
-                  beamPackages.elixir
-                  pkgs.just
-                ];
-
-                text = ''
-                  just mix all deps.get
-                  just mix all deps.nix
-                '';
-              };
-            in
-            {
-              type = "app";
-              program = lib.getExe script;
-            };
-
-          packages = rec {
-            default = expert;
-
-            expert = pkgs.callPackage ./nix/expert.nix { inherit beamPackages; };
-          };
-
-          beamWorkspace = {
-            enable = true;
-            devShell.languageServers.elixir = false;
-            devShell.languageServers.erlang = false;
-            versions = {
-              elixir = "1.17.3";
-              erlang = "27.3.4.1";
-            };
-            devShell.extraPackages = with pkgs; [
-              zig
-              xz
-              just
-              _7zz
-              git
+            runtimeInputs = [
+              beamPackages.elixir
+              pkgs.just
             ];
+
+            text = ''
+              just mix all deps.get
+              just mix all deps.nix
+            '';
           };
+        in {
+          type = "app";
+          program = lib.getExe script;
         };
+
+        packages = rec {
+          default = expert;
+
+          expert = pkgs.callPackage ./nix/expert.nix {inherit beamPackages;};
+        };
+
+        beamWorkspace = {
+          enable = true;
+          devShell.languageServers.elixir = false;
+          devShell.languageServers.erlang = false;
+          versions = {
+            elixir = "1.17.3";
+            erlang = "27.3.4.1";
+          };
+          devShell.extraPackages = with pkgs; [
+            alejandra
+            zig
+            xz
+            just
+            _7zz
+            git
+          ];
+        };
+      };
     };
 }
